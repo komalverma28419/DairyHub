d864385ee115c4168096dee9d9be0922990cc137
Komal Verma <komalverma28419@gmail.com>
2026-05-31 15:56:15 +0530

    Brand that trust us portion completed

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const menu = document.querySelector("#menu");
 const openBtn = document.querySelector("#openMenu");
 const closeBtn = document.querySelector("#closeMenu");
 
+const buttons = document.querySelectorAll(".tab-btn")
+const parts= document.querySelectorAll(".tab-content")
+
+
 function openMenu() {
     menu.classList.remove("opacity-0", "-translate-y-5", "pointer-events-none");
     openBtn.classList.add("hidden");
@@ -16,3 +20,27 @@ function closeMenu() {
 
 openBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
+
+
+// section-5
+buttons[0].classList.add("active");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const target = btn.dataset.tab;
+
+    // hide all
+    parts.forEach((part,index) => part.classList.add("hidden"));
+    document.getElementById(target)?.classList.remove("hidden");
+
+    // remove active from all buttons
+    buttons.forEach(b => b.classList.remove("active"));
+
+    // add active to clicked button
+    btn.classList.add("active");
+
+  });
+});
+
+
+
